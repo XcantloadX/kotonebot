24fd0c75e5364ec749788238effcd79956f51384
XcantloadX <3188996979@qq.com>
2025-12-17 14:09:43 +0800

    docs(fw): 为 Prefab 与 GameObject 对象补充 docstring

--- kotonebot/core/entities/base.py
+++ kotonebot/core/entities/base.py
@@ -19,6 +19,11 @@ class ClickKwargs(TypedDict, total=False):
 class Prefab(Generic[GameObjectType], ABC):
     __object_class__: Type[GameObjectType] | None = None
     display_name: str | None = None
+    """展示名称
+    
+    可选，用于在编辑器或日志中显示更友好的名称。
+    如果未设置，则使用类名。
+    """
 
     @classmethod
     def _get_object_class(cls) -> Type[GameObjectType]:
@@ -47,20 +52,57 @@ class Prefab(Generic[GameObjectType], ABC):
         return cast(Type[GameObjectType], GameObject)
 
     @classmethod
-    def find(cls, **kwargs: Unpack[PrefabKwargs[GameObjectType]]) -> GameObjectType | None: ...
+    def find(cls, **kwargs: Unpack[PrefabKwargs[GameObjectType]]) -> GameObjectType | None:
+        """在屏幕画面中寻找当前 Prefab，并返回对应的第一个 GameObject 实例。
+
+        :return: 寻找结果。如果没有找到，返回 None。
+        """
+        raise NotImplementedError
+    
     @classmethod
-    def find_all(cls, **kwargs: Unpack[PrefabKwargs[GameObjectType]]) -> list[GameObjectType]: ...
+    def find_all(cls, **kwargs: Unpack[PrefabKwargs[GameObjectType]]) -> list[GameObjectType]:
+        """在屏幕画面中寻找当前 Prefab，并返回对应的所有 GameObject 实例。
+
+        :return: 寻找结果列表。如果没有找到，返回空列表。
+        """
+        raise NotImplementedError
+    
     @classmethod
-    def require(cls, **kwargs: Unpack[PrefabKwargs[GameObjectType]]) -> GameObjectType: ...
+    def require(cls, **kwargs: Unpack[PrefabKwargs[GameObjectType]]) -> GameObjectType:
+        """在屏幕画面中寻找当前 Prefab，并返回对应的第一个 GameObject 实例。
+        
+        此方法与 find 类似，但如果没有找到任何结果，则会抛出异常。
+
+        :raises: 如果没有找到，抛出异常。
+        :return: 寻找结果。
+        """
+        raise NotImplementedError
+    
     @classmethod
-    def exists(cls, **kwargs: Unpack[PrefabKwargs[GameObjectType]]) -> bool: ...
+    def exists(cls, **kwargs: Unpack[PrefabKwargs[GameObjectType]]) -> bool:
+        """判断当前 Prefab 是否存在于屏幕画面中。
+        
+        此方法为 find 的简化版，仅返回是否存在。
+        相当于 ``Prefab.find(...) is not None``。
+        
+        :return: 如果存在，返回 True；否则返回 False。
+        """
+        return cls.find(**kwargs) is not None
 
     @classmethod
     def click(cls, **kwargs: Unpack[ClickKwargs]) -> None:
+        """在屏幕画面中寻找当前 Prefab，并点击第一个找到的 GameObject 实例。
+        
+        该方法会调用 require 方法，因此如果没有找到任何结果，则会抛出异常。
+        """
         return cls.require().click()
     
     @classmethod
     def try_click(cls, **kwargs: Unpack[ClickKwargs]) -> bool:
+        """尝试点击当前 Prefab 的第一个找到的 GameObject 实例。
+        
+        :return: 如果找到了对象并成功点击，返回 True；否则返回 False。
+        """
         obj = cls.find()
         if obj is not None:
             obj.click()
@@ -69,6 +111,10 @@ class Prefab(Generic[GameObjectType], ABC):
     
     @classmethod
     def wait(cls, *, timeout: float | None = None, interval: float | None = None, throw: bool = True):
+        """等待当前 Prefab 出现。
+        
+        若指定时间内未找到，则根据 throw 参数决定是抛出异常还是返回 None。
+        """
         start_time = time.time()
         while True:
             obj = cls.find()
@@ -85,6 +131,10 @@ class Prefab(Generic[GameObjectType], ABC):
                     
     @classmethod
     def try_wait(cls, *, timeout: float | None = None, interval: float | None = None):
+        """尝试等待当前 Prefab 出现。
+        
+        若指定时间内未找到，则返回 None。
+        """
         return cls.wait(timeout=timeout, interval=interval, throw=False)
 
 class GameObject:
@@ -94,22 +144,43 @@ class GameObject:
     GameObject 本身仅包含基础属性。如果你需要自定义 GameObject 的属性或行为，可以继承 GameObject 并使用你自己的类。
     """
     rect: Rect
+    """对象在屏幕上的范围"""
     display_name: str | None = None
+    """展示名称
+    
+    可选，用于在编辑器或日志中显示更友好的名称。
+    如果未设置，则使用类名。
+    """
 
     def click(self) -> None:
+        """点击当前对象的中心位置。"""
         from kotonebot import device
         device.click(self.rect.center)
 
     def double_click(self) -> None:
+        """双击当前对象的中心位置。"""
         from kotonebot import device
         device.double_click(*self.rect.center)
 
 class TemplateMatchPrefab(Prefab[GameObjectType]):
     """基于模版匹配的 Prefab"""
     template: Image
+    """[必填] 用于匹配的模版图像"""
     region: Rect | None = None
+    """[可选] 限定搜索区域
+    
+    默认为 None（全屏搜索）。
+    """
     threshold: float = 0.7
+    """[可选] 匹配阈值
+    
+    范围 0.0 - 1.0，默认为 0.7。
+    """
     colored: bool = False
+    """[可选] 是否匹配颜色
+    
+    默认为 False（不匹配颜色）。
+    """
 
     class _Editor(EditorMetadata):
         id = 'base_template_match'
@@ -191,12 +262,7 @@ class TemplateMatchPrefab(Prefab[GameObjectType]):
                     return obj
             # 没有任何匹配满足 predicate，抛出未找到异常
             raise TemplateNoMatchError(device.screenshot(), cls.template.pixels)
-    
-    @override
-    @classmethod
-    def exists(cls, **kwargs: Unpack[PrefabKwargs[GameObjectType]]) -> bool:
-        return cls.find(**kwargs) is not None
-        
+
 
 class OcrPrefab(Prefab[GameObjectType]):
     """基于 Ocr 的 Prefab"""
@@ -265,8 +331,3 @@ class OcrPrefab(Prefab[GameObjectType]):
                     if predicate(obj):
                         return obj
             raise TextNotFoundError(cls.pattern, device.screenshot())
-
-    @override
-    @classmethod
-    def exists(cls, **kwargs: Unpack[PrefabKwargs]) -> bool:
-        return cls.find(**kwargs) is not None
