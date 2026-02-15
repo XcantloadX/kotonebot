c133b55f142d3f1549305d79309944fee0026de3
XcantloadX <3188996979@qq.com>
2026-02-15 09:00:06 +0800

    feat(devtool): 为打开文件对话框新增 Tree View

--- kotonebot-devtools2/src/editor/settings.ts
+++ kotonebot-devtools2/src/editor/settings.ts
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
-export type FileDialogViewMode = "list" | "thumb";
+export type FileDialogViewMode = "list" | "thumb" | "tree";
 
 interface SettingsState {
   fileDialogViewMode: FileDialogViewMode;
